45fdf119b6a743fdc91467fb476be4581547412f
Daniel <raiga1234@gmail.com>
2020-07-20 16:06:43 -0500

    Avatar page custom

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -4,7 +4,34 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar Page')),
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://fotos02.laopiniondemalaga.es/2018/11/24/1024x341/stan-lee.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://hipertextual.com/files/2019/04/hipertextual-avengers-endgame-contiene-ultimo-cameo-stan-lee-2019632812-scaled.jpg'),
+          placeholder: AssetImage('data/assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
