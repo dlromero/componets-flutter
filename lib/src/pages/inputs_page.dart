92fd19aba8a1a728103e5a37f9bad27beef74e9f
Daniel <raiga1234@gmail.com>
2020-07-20 21:11:41 -0500

    Create Select Datepicker

--- lib/src/pages/inputs_page.dart
+++ lib/src/pages/inputs_page.dart
@@ -8,7 +8,12 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _name;
+  String _name = '';
+  String _email = '';
+  String _password = '';
+  String _date = '';
+
+  TextEditingController _inputFieldDateControler = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +23,17 @@ class _InputPageState extends State<InputPage> {
       ),
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        children: <Widget>[_createInput(), Divider(), _createPeople()],
+        children: <Widget>[
+          _createInput(),
+          Divider(),
+          _createEmail(),
+          Divider(),
+          _createPassword(),
+          Divider(),
+          _createDate(context),
+          Divider(),
+          _createPerson(),
+        ],
       ),
     );
   }
@@ -29,7 +44,7 @@ class _InputPageState extends State<InputPage> {
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
-          counter: Text('Letras ${_name.length}'),
+          counter: Text('Letras ${_name != null ? _name.length : 0}'),
           hintText: 'Nombre de la persona',
           labelText: 'Nombre',
           helperText: 'Sólo es el nombre',
@@ -39,9 +54,56 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
-  Widget _createPeople() {
+  Widget _createPerson() {
     return ListTile(
       title: Text('Nombre es: $_name'),
+      subtitle: Text('Correo es: $_email'),
+    );
+  }
+
+  Widget _createEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Correo de la persona',
+          labelText: 'Correo',
+          helperText: 'example@example.com',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (value) => _changeEmail(value),
+    );
+  }
+
+  Widget _createPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Password de la persona',
+          labelText: 'Password',
+          helperText: '*********',
+          suffixIcon: Icon(Icons.lock_open),
+          icon: Icon(Icons.lock)),
+      onChanged: (value) => _changePassword(value),
+    );
+  }
+
+  Widget _createDate(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateControler,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          hintText: 'Fecha de nacimiento',
+          labelText: 'Seleccione fecha',
+          helperText: '1/1/2020',
+          suffixIcon: Icon(Icons.perm_contact_calendar),
+          icon: Icon(Icons.calendar_today)),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
     );
   }
 
@@ -49,5 +111,32 @@ class _InputPageState extends State<InputPage> {
     setState(() {
       _name = name;
     });
+  }
+
+  void _changeEmail(String email) {
+    setState(() {
+      _email = email;
+    });
+  }
+
+  void _changePassword(String password) {
+    setState(() {
+      _password = password;
+    });
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2020),
+        lastDate: new DateTime(2025),
+        locale: Locale('es', 'CO'));
+    if (picked != null) {
+      setState(() {
+        _date = picked.toString();
+        _inputFieldDateControler.text = _date;
+      });
+    }
   }
 }
