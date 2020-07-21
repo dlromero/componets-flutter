91b7d1955f9ea848e8954783431da17b8ac73252
Daniel <raiga1234@gmail.com>
2020-07-20 20:35:29 -0500

    Add new route and Input field

---
+++ lib/src/pages/inputs_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  InputPage({Key key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs de texto'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: <Widget>[_createInput(), Divider(), _createPeople()],
+      ),
+    );
+  }
+
+  Widget _createInput() {
+    return TextField(
+      // autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letras ${_name.length}'),
+          hintText: 'Nombre de la persona',
+          labelText: 'Nombre',
+          helperText: 'Sólo es el nombre',
+          suffixIcon: Icon(Icons.accessibility),
+          icon: Icon(Icons.account_circle)),
+      onChanged: (value) => _changeName(value),
+    );
+  }
+
+  Widget _createPeople() {
+    return ListTile(
+      title: Text('Nombre es: $_name'),
+    );
+  }
+
+  void _changeName(String name) {
+    setState(() {
+      _name = name;
+    });
+  }
+}
